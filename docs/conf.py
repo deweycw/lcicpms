aa0f896e054cb302dd9105e3a4188db97e02ea87
Christian Dewey <dewey.christian@gmail.com>
2024-09-06 17:12:30 -0500

    adding docs dir

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'lcicpms'
+copyright = '2024, Christian Dewey & Rene Boiteau'
+author = 'Christian Dewey & Rene Boiteau'
+release = '0.0.1'
+
+import os
+import sys
+sys.path.insert(0,os.path.abspath('..'))
+# sys.path.append(os.path.abspath('/Users/christiandewey/CoreMS'))
+# sys.path.append(os.path.abspath('/Users/christiandewey/CoreMS/ext_lib'))
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc','sphinx.ext.coverage','sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'Python'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+
+autodoc_mock_imports = ["corems", "numpy", "scipy", "pandas", "tqdm", "matplotlib.pyplot", "matplotlib","seaborn"]
