91b1f3acabb0aa457bd344c6b72ca4ebd4eb0fe2
Christian Dewey <dewey.christian@gmail.com>
2024-09-06 13:18:01 -0500

    added helptext

--- src/lcicpms/integrate.py
+++ src/lcicpms/integrate.py
@@ -5,75 +5,51 @@
 from numpy import where
 
 class Integrate:
+    """
+    A class to handle integration of signal traces over a given time range.
+    """
 
-    def integrate(intensities, times, time_range : tuple = None):
-        '''
-        Returns area under signal trace curve between specified time range. 
-        '''
+    @staticmethod
+    def integrate(intensities, times, time_range: tuple = None):
+        """
+        Computes the area under the signal trace curve between a specified time range.
 
-        if time_range == None:
+        This function performs trapezoidal integration on the provided intensities and times,
+        returning the total area within the time range. If no time range is provided, it will
+        integrate over the entire signal.
+
+        Args:
+            intensities (array-like): The intensities corresponding to each time point.
+            times (array-like): The time points at which the intensities were measured.
+            time_range (tuple, optional): The start and end times for the integration. 
+                                          If None, the entire signal is integrated. Defaults to None.
+
+        Returns:
+            float: The computed area under the curve between the specified time range.
+        """
+        if time_range is None:
             i_tmin = 0
-            i_tmax = len(times)-2
-
+            i_tmax = len(times) - 2
         else:
-            i_tmin = int(where(abs(times - time_range[0]) == min(abs(times - time_range[0])) )[0][0])
-            i_tmax = int(where(abs(times - time_range[1]) == min(abs(times - time_range[1])) )[0][0])
+            i_tmin = int(where(abs(times - time_range[0]) == min(abs(times - time_range[0])))[0][0])
+            i_tmax = int(where(abs(times - time_range[1]) == min(abs(times - time_range[1])))[0][0])
 
         peak_area = 0
         dt = 0
 
         for i in range(i_tmin, i_tmax):
-            
             dt1_intensities = intensities[i]
-            dt2_intensities = intensities[i+1]
+            dt2_intensities = intensities[i + 1]
             
-            dt_I_min = min([dt1_intensities,dt2_intensities])
-            dt_I_max = max([dt1_intensities,dt2_intensities])
+            dt_I_min = min([dt1_intensities, dt2_intensities])
+            dt_I_max = max([dt1_intensities, dt2_intensities])
             
-            dt = (times[i+1] - times[i])
+            dt = (times[i + 1] - times[i])
 
             rect_area = dt * dt_I_min
             top_area = dt * (dt_I_max - dt_I_min) * 0.5
             d_peak_area = rect_area + top_area
 
-            peak_area = peak_area + d_peak_area  
+            peak_area += d_peak_area
 
         return peak_area
-
-    def other():   
-        #print('yes base subtract')
-        if self._view.baseSubtract == True:
-            #print('yes base subtract')
-            baseline_height_1 = self._data.iloc[i_tmin,me_col_ind] / corr_factor
-            baseline_height_2 =  self._data.iloc[i_tmax,me_col_ind] / corr_factor
-            baseline_timeDelta = (self._data.iloc[i_tmax,me_col_ind - 1] - self._data.iloc[i_tmin,me_col_ind - 1])/60 #minutes
-            #print('baseline_height_1: %.2f' % baseline_height_1)
-            #print('baseline_height_2: %.2f' % baseline_height_2)
-            #print('timeDelta: %.2f' % baseline_timeDelta)
-
-            min_base_height = min([baseline_height_1, baseline_height_2])
-            max_base_height = max([baseline_height_1, baseline_height_2])
-            #print('min_base_height: %.2f' % min_base_height)
-            #print('max_base_height: %.2f' % max_base_height)
-            baseline_area_1 = min_base_height * baseline_timeDelta
-            baseline_area_2 = (max_base_height - min_base_height) * baseline_timeDelta * 0.5
-            #print('baseline_area_1: %.2f' % baseline_area_1)
-            #print('baseline_area_2: %.2f' % baseline_area_2)
-            
-            #print('summed_area: %.2f' % summed_area)
-            baseline_area = baseline_area_1 + baseline_area_2
-            summed_area = summed_area - baseline_area
-            summed_area = max(summed_area,0)
-            #print('baseline_area: %.2f' % baseline_area)
-            
-
-        cal_curve = self._view.calCurves[metal]	
-        slope = cal_curve['m']
-        intercept = cal_curve['b']
-        conc_ppb = slope * summed_area + intercept
-        conc_uM = conc_ppb / self._view.masses[metal]
-        
-        peakAreas[metal] = '%.1f' % summed_area
-        metalConcs[metal] = '%.3f' % conc_uM
-        print('\n' + metal + ' uM: %.3f' % conc_uM)
-        print(metal  + ' peak area: %.1f' % summed_area)
--- src/lcicpms/quantitate.py
+++ src/lcicpms/quantitate.py
@@ -7,23 +7,51 @@ from lcicpms.raw_icpms_data import RawICPMSData
 from lcicpms.calibration import Calibration
 from lcicpms.in_out import export_df
 from lcicpms.integrate import Integrate
-
 import lcicpms.utils as utils
 
 class Quantitate:
+    """
+    A class to perform quantitation of elements using LC-ICP-MS data. It applies
+    calibration curves to raw LC-ICP-MS data to compute concentrations over a given time range.
+    """
 
-    def run(raw_icpms_data : RawICPMSData = None, 
-            calibration : Calibration = None,
-            elements : list = [], 
-            time_range : tuple = (-1,-1), 
-            istd_baseline : float = -1.0, 
-            istd_time_range : tuple = (-1,-1),
-            baseline_subtraction : bool = False, 
-            unit : str = 'ppb'):
-        '''
-        Runs the quantiation over specified time range for passed elements. Returns dictionary with elements as keys, concentrations as values.
-        '''
+    def run(raw_icpms_data: RawICPMSData = None, 
+            calibration: Calibration = None,
+            elements: list = [], 
+            time_range: tuple = (-1, -1), 
+            istd_baseline: float = -1.0, 
+            istd_time_range: tuple = (-1, -1),
+            baseline_subtraction: bool = False, 
+            unit: str = 'ppb'):
+        """
+        Perform quantitation for a specified list of elements over a time range.
 
+        Args:
+            raw_icpms_data (RawICPMSData): An instance of RawICPMSData containing 
+                                           raw ICP-MS intensities and times.
+            calibration (Calibration): An instance of Calibration containing 
+                                       calibration curves for elements.
+            elements (list): A list of element names (strings) to be quantified.
+            time_range (tuple): A tuple representing the start and end times 
+                                (in seconds) for quantitation. Defaults to (-1, -1),
+                                which indicates using the entire time range.
+            istd_baseline (float): The baseline value for internal standard correction. 
+                                   If > 0, internal standard correction is applied.
+                                   Defaults to -1.0 (no correction).
+            istd_time_range (tuple): A tuple representing the time range for the 
+                                     internal standard. Defaults to (-1, -1).
+            baseline_subtraction (bool): If True, baseline subtraction is applied. 
+                                         Defaults to False.
+            unit (str): The unit of the concentration (e.g., 'ppb'). Defaults to 'ppb'.
+
+        Returns:
+            dict: A dictionary containing:
+                - 'areas': DataFrame of integrated peak areas for each element.
+                - 'concentrations': DataFrame of concentrations calculated for each element.
+                - 'internal_std_correction': The internal standard correction factor applied.
+        
+        """
+        
         intensities = raw_icpms_data.intensities
         times = raw_icpms_data.times
 
